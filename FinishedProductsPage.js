e138899e03bb80c977f803ffc6969da74cace006
gobananabean <57016874+gobananabean@users.noreply.github.com>
2019-11-08 15:19:48 -0500

    Class

---
+++ FinishedProductsPage.js
@@ -0,0 +1,5 @@
+function changeStyle () {
+	alert("Thank you for filling out this survey!")
+	var text = document.getElementById("submit").style.color="yellow";
+
+}
---
+++ StainedGlassPage (2).js
@@ -0,0 +1,20 @@
+function newPicture() {
+
+	document.getElementById("bluewave").src="orangewave.jpg";
+}
+function oldPicture() {
+
+	document.getElementById("bluewave").src="bluewave.jpg";
+}
+function changeStyle () {
+
+	document.getElementById("email").innerHTML = "Your email has been sent!";
+}
+
+
+
+
+
+
+
+
